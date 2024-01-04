608d1d7ae8cc8b2d92dcda00d7aaeda2311bd3b0
Suraj Kumar <surajkmr20167@gmail.com>
2024-01-04 07:28:30 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1234;
+
+let accountName = "Suraj";
+
+var accountPassword = "suraj123";
+
+/*
+    Prefer not to use var. 
+    Beacause it doesn't have block scope or functional scope.
+*/
+
+email = "suraj@gmail.com";   // Don't use this. // Just keep in mind that js can do this.
+
+//accountId = 111;  // *Cannot assign value to a constant.
+
+console.table([ accountId, accountName, accountPassword, email]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"  // Treat all JS code as newer version.
+
+// alert("Hello"); // will not work in nodeJS, will work in browser.
+
+let age = 24;
+let name =  "suraj";
+let test = true;
+
+
+// number
+// bigInt
+// string
+// boolean
+// null => standalone value
+// undefined
+// symbol
+
+console.log(typeof "hello");
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
