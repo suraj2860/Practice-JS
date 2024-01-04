898dbe42c42067e044cf174259cc65bcf237a784
Suraj Kumar <surajkmr20167@gmail.com>
2024-01-04 07:45:24 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "Suraj"; 
+
+let temp = Number(score);
+
+console.log(typeof temp);
+console.log(temp);
+
+// ------Conversion to number-------
+// "33" => 33
+// "suraj" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = "suraj";
+
+let boolIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof boolIsLoggedIn);
+console.log(boolIsLoggedIn);
+
+// -------Conversion to boolean--------
+// 1 => true; 0 => false
+// "" => false
+// "suraj" => true
+
+
