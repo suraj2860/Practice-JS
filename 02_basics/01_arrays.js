cd08c349157aea08ba7369745c9194eeebdc3f5b
Suraj Kumar <surajkmr20167@gmail.com>
2024-01-05 05:37:06 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+// array 
+
+const arr = [1, 2, 3, 4, 5];
+const myArr = ["suraj", "harshit", "sagar", "sourav"];
+
+const myArr2 = new Array(3, 2, "suraj", { id: 2, name: "suraj"}, null, undefined, true);
+
+console.log(myArr2[3].name);
+
+
+// Array methods
+
+myArr2.push(6);  // Adds element to the last.
+myArr2.unshift("sourav");   // Adds element to the start.
+
+console.log(myArr2);
+
+myArr2.pop();   // removes element from last.
+myArr2.shift();     // removes element from start.
+
+console.log(myArr2);
+
+
+// # Slice And splice
+
+const anotherArray = myArr2.slice(1,3);     // returns an array with elements [1,3). without affecting the original array.
+
+console.log("sliced array : ", anotherArray);
+console.log("original array : ", myArr2);
+
+const anotherArray2 = myArr2.splice(1,3);   // returns an array with elements [1,3]. it removes the elements from original array.
+
+console.log("spliced array : ", anotherArray2);
+console.log("original array : ", myArr2);
