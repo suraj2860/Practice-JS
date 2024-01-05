3594b49369b7a67a2827d8c26491f5b8a42197a3
Suraj Kumar <surajkmr20167@gmail.com>
2024-01-05 11:18:48 +0000

    Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,35 @@
+const myObj = new Object();  // we can object like this also!!
+
+const obj = {};
+
+obj.id = "123";
+obj.name = "suraj";
+obj.email = "suraj@google.com";
+
+console.log(myObj);
+
+
+//Merging two objects
+
+const obj1 = {1:"a", 2:"b"};
+const obj2 = {4:"a", 3:"b"};
+
+const obj3 = {obj1, obj2};  // this is wrong. it will create nested objects.
+
+const obj4 = Object.assign({}, obj1, obj2);  // this is correct way.
+
+// there is one more way!! 
+
+const obj5 = {...obj1, ...obj2};
+
+console.log(obj5);
+
+
+// Obkect Methods
+
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+console.log(Object.entries(obj));
+console.log(obj.hasOwnProperty("name"));
+
+
