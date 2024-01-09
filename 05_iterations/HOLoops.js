e5d7af7ea2a229a181443269b107b9d6673fe6c6
Suraj Kumar <surajkmr20167@gmail.com>
2024-01-09 03:56:29 +0000

    High order array loops

---
+++ 05_iterations/HOLoops.js
@@ -0,0 +1,65 @@
+// for of loop
+
+const arr = [1,2,3,4,5];
+
+const mapTest = new Map();
+mapTest.set("name","suraj");
+mapTest.set("age",24);
+mapTest.set("location","Roorkee");
+mapTest.set("email","suraj@google.com");
+
+const myObj = {
+    js: "javaScript",
+    py: "python",
+    cpp: "C++"
+};
+
+
+for(const val of arr){
+    console.log(val);
+}
+
+for(const [key,value] of mapTest){
+    console.log(key + " - " +  value);
+}
+
+// for(const val of myObj){
+//     console.log(val);       // myObj is not iterable.
+// }
+
+// for in loop : returns the key.
+
+for(const val in arr){
+    console.log(val);
+}
+
+for(const val in mapTest){      // map is notiterable using for in
+    console.log(val);       
+}
+
+for(const val in myObj){
+    console.log(myObj[val]);       
+}
+
+
+// forEach  Loop
+
+const coding = ["js", "py", "C#", "C++", "java"];
+
+coding.forEach(function (item){
+    console.log(item)
+});
+
+coding.forEach( (item) => {
+    console.log(item)
+});
+
+coding.forEach( (item, index, arr) => {
+    console.log(`value : ${item} , index : ${index}, array : ${arr}`);
+});
+
+function printMe(){
+    console.log("Hello");
+}
+
+coding.forEach(printMe);
